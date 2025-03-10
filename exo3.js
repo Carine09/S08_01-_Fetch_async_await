d519f41d9787efa0041caeb8881a7c2a591f0db3
Carine <carinerandrianarijaona@gmail.com>
2025-03-10 15:50:52 +0100

    Exercice 3

--- exo3.js
+++ exo3.js
@@ -2,3 +2,26 @@
 
 Pour cet exercice, laisse libre cours à ton imagination. 💭 */
 
+const citationContainer = document.querySelector("#citationContainer");
+
+async function fetchCitations() {
+    const response = await fetch("https://www.dummyjson.com/quotes");
+    const citations = await response.json();
+    console.log(citations);
+    // console.log(offers[0].titre);
+    
+    citationContainer.innerText = "";
+
+    citations.quotes.forEach(citation => {
+        let quoteContainer = document.createElement("div");
+        quoteContainer.innerText = citation.quote;
+        citationContainer.appendChild(quoteContainer);
+
+        let authorContainer = document.createElement("h3");
+        authorContainer.innerText = citation.author;
+        citationContainer.appendChild(authorContainer);
+    });
+}
+
+citationContainer.innerText = `Chargement des citations...`
+fetchCitations();
